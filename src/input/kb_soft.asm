a3daddba005cd95114c8c51a821fb0ac8573f66f
freem <aj187@ajworld.net>
2015-09-16 10:45:28 -0500

    software keyboard musings (small keyboard.txt update to note that it's about hardware keyboards)

--- src/input/kb_soft.asm
+++ src/input/kb_soft.asm
@@ -8,6 +8,23 @@
 
 ; Enter the software keyboard. Unlike the hardware keyboards, the software
 ; keyboard uses an on-screen display for input.
+
+; (Unshifted Layout)
+; 1 2 3 4 5 6 7 8 9 0 - = BSPC
+;  Q W E R T Y U I O P [ ] \
+;   A S D F G H J K L ; ' ENTR
+; FN Z X C V B N M , . / SHIFT
+; ESC  CTRL  SPACE  ALT  CLOSE
+
+; (Shifted Layout)
+; ! @ # $ % ^ & * ( ) _ + BSPC
+;  Q W E R T Y U I O P [ ] \
+;   A S D F G H J K L : " ENTR
+; FN Z X C V B N M < > ? SHIFT
+; ESC  CTRL  SPACE  ALT  CLOSE
+
+;==============================================================================;
+; Routine naming: softkb_*
 ;==============================================================================;
 KBSignature_Soft:	.db "JOYKB"
 ;==============================================================================;
