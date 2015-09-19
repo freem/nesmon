53ca8d288fc06153474f83363bed4bc181aa8272
freem <aj187@ajworld.net>
2015-09-19 15:59:59 -0500

    fleshing out chrview a bit more

--- src/modules/chrview.asm
+++ src/modules/chrview.asm
@@ -7,83 +7,161 @@
 ;==============================================================================;
 ; Routine naming: chrview_*
 ;==============================================================================;
+; [Module RAM definitions]
+; you'd think these would be GOOD to have in the NL, but conflicting addresses
+; make this a bummer.
+.ignorenl
+	CHRVIEW_CURSORPOS = moduleRAM		; (1 byte)
+	CHRVIEW_TILEBUF_EDIT = moduleRAM+$10	; (16 bytes)
+	CHRVIEW_TILEBUF_UNDO = moduleRAM+$20	; (16 bytes)
+.endinl
+
+;==============================================================================;
+; [Tables]
+
+; nametable addresses for cursors
+tbl_chrview_MenuCursorAddr:
+	.db $23,$01		;  0: Colors
+	.db $23,$21		;  1: CHRBANK
+	.db $23,$41		;  2: SWAPCOL
+	.db $23,$0A		;  3: HFlip
+	.db $23,$2A		;  4: VFlip
+	.db $23,$4A		;  5: Clear
+	.db $23,$11		;  6: Cut
+	.db $23,$31		;  7: Copy
+	.db $23,$51		;  8: Paste
+	.db $23,$18		;  9: Undo
+	.db $23,$38		; 10: Option
+	.db $23,$58		; 11: Exit
 
 ;==============================================================================;
 ; [Strings]
 
-; todo: nametable addresses are obviously wrong
-vbstr_chrview_PixColor:
-	.db $20,$00,6
-	.db "PIXEL COLOR"
+str_chrview_HeaderText:		.db "CHR DATA VIEWER/EDITOR"
 
+vbstr_chrview_PixColor:
+	.db $22,$14,6,"PIXEL COLOR"
+
+; --Menu Choices--
 vbstr_chrview_Colors:
-	.db $20,$00,6
-	.db "COLORS"
+	.db $23,$02,6,"COLORS"
 
 vbstr_chrview_CHRBank:
-	.db $20,$00,7
-	.db "CHRBANK"
+	.db $23,$22,7,"CHRBANK"
 
-vbstr_chrview_Col1Row3:
-	.db $20,$00,7
-	.db "???????"
+vbstr_chrview_SwapCol:
+	.db $23,$42,7,"SWAPCOL"
 
 vbstr_chrview_HFlip:
-	.db $20,$00,5
-	.db "HFLIP"
+	.db $23,$0B,5,"HFLIP"
 
 vbstr_chrview_VFlip:
-	.db $20,$00,5
-	.db "VFLIP"
+	.db $23,$2B,5,"VFLIP"
 
 vbstr_chrview_Clear:
-	.db $20,$00,5
-	.db "CLEAR"
+	.db $23,$4B,5,"CLEAR"
 
 vbstr_chrview_Cut:
-	.db $20,$00,3
-	.db "CUT"
+	.db $23,$12,3,"CUT"
 
 vbstr_chrview_Copy:
-	.db $20,$00,4
-	.db "COPY"
+	.db $23,$32,4,"COPY"
 
 vbstr_chrview_Paste:
-	.db $20,$00,5
-	.db "PASTE"
+	.db $23,$52,5,"PASTE"
 
-vbstr_chrview_Col4Row1:
-	.db $20,$00,7
-	.db "???????"
+vbstr_chrview_Undo:
+	.db $23,$19,4,"UNDO"
 
 vbstr_chrview_Option:
-	.db $20,$00,6
-	.db "OPTION"
+	.db $23,$39,6,"OPTION"
 
 vbstr_chrview_Exit:
-	.db $20,$00,4
-	.db "EXIT"
+	.db $23,$59,4,"EXIT"
+
 ;==============================================================================;
 chrview_Init:
-	; blah
+	; variable initialization
+	lda #0
+	sta CHRVIEW_CURSORPOS
+
+	; you need to turn off de light
 
 	; clear nametables
 	
 
 	; set up screen display
+	jsr chrView_InitDisplay
+
 	; clear module ram
+	jsr nesmon_ClearModuleRAM
+
+	; ok turn it back on again
 
 ;------------------------------------------------------------------------------;
 chrview_MainLoop:
+	; --before vblank--
+
+	; --vblank--
+	jsr ppu_WaitVBlank
+
+	; --after vblank--
+	jsr chrview_Input
+
 	jmp chrview_MainLoop
 
 ;==============================================================================;
 chrview_Input:
+	;
+
 	rts
+
+;==============================================================================;
+chrView_InitDisplay:
+	; --header--
+	; line at $2020
+	; str_chrview_HeaderText
+	; line at $2060
+
+	; main view
+
+	; zoomed view
+
+	; "pixel color" area
+
+	; main menu
+
+	rts
+
 ;==============================================================================;
 chrview_ClearMenu:
 	rts
+
 ;==============================================================================;
 chrview_DrawMainMenu:
 	rts
+
 ;==============================================================================;
+; chrview_ChangeTile
+; Called upon changing the active tile to view/edit
+
+chrview_ChangeTile:
+	rts
+
+;==============================================================================;
+; chrview_UpdateTileData
+; Write back the changes to the tile
+
+chrview_UpdateTileData:
+	; save current tile state before changing
+
+	; change tile data
+
+	rts
+
+;==============================================================================;
+; chrview_Undo
+; Undoes the last action (by writing the previous tile's data)
+
+chrview_Undo:
+	rts
